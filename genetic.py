86119185470abf3c17a792ea77a7ce62e4d2d4c4
Guari <renzoguari99@gmail.com>
2021-05-15 18:14:44 -0300

    HOla

--- genetic.py
+++ genetic.py
@@ -5,20 +5,117 @@ from simulated_anneling import anneling,layout, ley_enfriamiento
 
 class genetic():
 
-    def __init__(self,_cant_poblacion):
+    def __init__(self,_cant_poblacion,_list_ordenes,_obstaculos,_layout,_T):
         self.cant_poblacion = _cant_poblacion #Numeros de individuos de una poblacion
         self.poblacion=[]                     #Poblacion inicial 
+        self.colm=12
+        self.row=9
         #Creacion de la poblacion incial 
         self.set_poblacion()
+        self.list_ordenes=_list_ordenes
+        self.obstaculos=_obstaculos
+        self.layout=_layout
+        self.n=len(self.poblacion)
+        self.T=_T   #Agenda de enfriamiento
 
-    def genoma(self,colm,row):   
+    def genoma(self):
+
         individuo=[]
-        for i in range(row):
-            individuo.append([random.sample(range(100),colm)])
+        aux=random.sample(range(1,109),108)
+        for i in range(self.row):
+
+            individuo.append(aux[i*self.colm:(i+1)*self.colm])
+
+        return individuo
     
     def set_poblacion(self):
         for i in range(self.cant_poblacion):
-            self.poblacion.append(self.genoma(12,9))
+            self.poblacion.append(self.genoma())
 
-    def fitness(self):
-        pass
+    #La funcion de fitness hace uso del temple simulado para obtener una medida de idoneidad
+    def fitness(self,individuo):
+        #Se busca las coordenadas de estos puntos en el layout
+        
+        fit=0
+        for order in self.list_ordenes :
+            list_order2=[]
+            for q2 in order:
+
+                it=0
+
+                for q in individuo :
+
+
+                    if q2 in q:
+
+                        a=q.index(q2)
+                        aux=self.obstaculos[it*(len(q)-1)+a]
+                        list_order2.append(aux)
+
+                    it +=1
+
+            temple=anneling(list_order2,self.T,self.obstaculos,2,[0,0],[0,0],fin=True)
+            [way,resultado,E]=temple.search()
+            fit+=fit+E[-1]
+            
+        return  fit/len(self.list_order)
+
+    def cross_over(self,cant_cross=1):
+        
+        pares=[[random.sample(self.poblacion_best,2)] for i in range(round(self.n)/2)]
+        self.poblacion=[]
+
+        for i in range(round(self.n)/2):
+            
+            index_colm=[[0,0]]
+            index_row=[[0,0]]
+            index_colm.append([random.sample(self.colm,cant_cross)])
+            index_row.append([random.sample(self.row,cant_cross)])
+            individuo_aux1=pares[i][1]
+            individuo_aux2=pares[i][2]
+            aux1=[]
+            aux2=[]
+
+            for j in range(cant_cross):
+
+                if j%2==0:
+
+                    aux1.append(individuo_aux2[index_row[j]:index_row[j+1]][index_colm[j]:index_colm[j+1]])
+                    aux2.append(individuo_aux1[index_row[j]:index_row[j+1]][index_colm[j]:index_colm[j+1]])
+
+                else:
+
+                    aux1.append(individuo_aux1[index_row[j]:index_row[j+1]][index_colm[j]:index_colm[j+1]])
+                    aux2.append(individuo_aux2[index_row[j]:index_row[j+1]][index_colm[j]:index_colm[j+1]])  
+
+            self.poblacion.append(aux1,aux2)    
+
+
+    def process(self):
+        
+        #Se selecciona los k mejores individuos
+        it=0
+        while it<1000:
+
+            k=round(self.n*0.4)
+            list_fit=[]
+            self.poblacion_best=[]
+
+            for invididuos in self.poblacion:
+
+                list_fit.append(self.fitness(invididuos))
+
+            dictor=dict.fromkeys(list_fit,range(self.cant_poblacion))      
+
+            list_fit.sort()
+            
+            for i in list_fit:
+                self.poblacion_best.append(dictor[i])
+
+            self.cross_over()
+
+            it+=1
+
+        return self.poblacion_best[-1]
+        
+        
--- punto4.py
+++ punto4.py
@@ -2,7 +2,7 @@
 #Implementar un algoritmo genético para resolver el problema de optimizar la ubicación de los
 #productos en el almacén, de manera de optimizar el picking de los mismos. Considere que
 
-from genetic import genetic, genoma
+from genetic import genetic
 import random
 import colorama
 import numpy as np
@@ -28,14 +28,25 @@ with open('orders.txt') as archivo:
         list_aux2=list(map(int,list_aux2))
         list_order.append(list_aux2)
 
+#Parametros del modelo
+len_enfria=200  #Longitud de la ley de enfriamiento
+coef_exp=1.5    #Coef de caida exponelcia de la temperatura
+tem_max=5000    #Temperatur maxima
+cant_ordenes=1  #Cantidad de ordenes
+len_ordenes=5  #Longitud de ordenes
+
 lista_A=[]      #Lista que contiene el mapeo del layout
 osbtaculos=[]   #Lista que contiene las direccion de las estanterias dentro de lista_A, que van a ser consideradas como obstaculos por nuestro algoritmo A*
 
 # Se crea el layout asignando un numero a cada estanteria y con '*' a los pasillos
 [lista_A,osbtaculos]=layout()
 
+T=ley_enfriamiento(tem_max,len_enfria,coef_exp)
 #Creacion de la poblacion incial
 
 cant_poblacion = 10 #Numeros de individuos de una poblacion
-alg_genetic=genetic(cant_poblacion)
+alg_genetic=genetic(cant_poblacion,list_order,osbtaculos,lista_A,T)
+print(alg_genetic.process())
+
+
 
