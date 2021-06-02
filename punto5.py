62f5ef20dcddc4548b836804a994b3baade943e8
lucas-trubiano <lucas.trubiano@gmail.com>
2021-06-02 12:20:52 -0300

    aca esta el planteo

--- punto5.py
+++ punto5.py
@@ -1,11 +1,20 @@
 # Satisfaccion de restricciones
-# Por donde empezar?
+# Problema: ====================================================
+
 # al inicio hay que definir una lista de tareas
-# cada tarea tiene:
+# cada TAREA tiene:
 # - Identificacion
 # - Duracion
 # - Requerimientos de Maquina (Tipo)
+
 # ademas hay que cargar las maquinas que tenemos
-# cada maquina tiene:
+# cada MAQUINA tiene:
 # - Identificador
 # - Tipo
+
+# Variables: ====================================================
+# TSi: periodo en que se inicia la tarea (num entero de periodos ej horas)
+# TMi: lista de maquinas del tipo que se requieran (puede haber mas de una maquina de ese tipo)
+
+# Restricciones: ====================================================
+# una restriccion Cr involucra 4 variables (TSi,TMi,TSj,TMj)
