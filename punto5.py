a958b716a07e84dd65571d2a9fa4d1067a219a4b
lucas-trubiano <lucas.trubiano@gmail.com>
2021-06-02 12:16:08 -0300

    consignas

--- punto5.py
+++ punto5.py
@@ -1,2 +1,11 @@
 # Satisfaccion de restricciones
 # Por donde empezar?
+# al inicio hay que definir una lista de tareas
+# cada tarea tiene:
+# - Identificacion
+# - Duracion
+# - Requerimientos de Maquina (Tipo)
+# ademas hay que cargar las maquinas que tenemos
+# cada maquina tiene:
+# - Identificador
+# - Tipo
