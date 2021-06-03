df88081d232be477a3d713dacc69027ba3fc56d4
lucas-trubiano <lucas.trubiano@gmail.com>
2021-06-02 21:38:51 -0300

    generacion aleatoria mas variada

--- punto5.py
+++ punto5.py
@@ -2,19 +2,18 @@
 #scheduling. El problema de scheduling consiste en asignar recursos a tareas. Modele las
 #variables, dominio de las mismas, restricciones, etc. Asuma que
 
-#Datos:     .Tares:  -id: identificacion de la tarea
-#                    -M: maquinas que pueden utilizar
-#                    -T: tiempo que se demora (en minutos)
-#           .Maquinas:   -id
-#                        -tipo
+# Datos:     .Tares:  -id: identificacion de la tarea
+#                     -M: maquinas que pueden utilizar
+#                     -T: tiempo que se demora (en minutos)
+#            .Maquinas:   -id
+#                         -tipo
 
-#Variables:     .TS: tiempo de inicio de una tarea
-#               .TM: tipo de maquina utilizable
+# Variables:     .TS: tiempo de inicio de una tarea
+#                .TM: tipo de maquina utilizable
 
-#Suposiciones: -Cada tarea se realiza con una sola maquina
-#              -Cada maquina puede hacer una sola tarea 
-#              -Pueden haber 2 o mas maquinas iguales 
-#
+# Suposiciones: -Cada tarea se realiza con una sola maquina
+#               -Cada maquina puede hacer una sola tarea 
+#               -Pueden haber 2 o mas maquinas iguales 
 
 from random import randint
 
@@ -24,44 +23,27 @@ max_duracionTarea=20 #min
 
 #Variables
 tiposmaquinas=['torno','amoladora','mezcladora','trituradora','alesadora','fresadora','CNC','oxicorte','impresora 3D','soldadora']
-# maquinas=[{'id':i,'tipo':tiposmaquinas[randint(0,len(tiposmaquinas)-1)]} for i in range(cant_maquinas)]
-# maquinas=[{'id':i,'tipo':tiposmaquinas[randint(0,len(tiposmaquinas)-1)]} for i in range(cant_maquinas)]
 maquinas = []
 for i in range(cant_maquinas):
     while True:
         maqAleatoria = tiposmaquinas[randint(0,len(tiposmaquinas)-1)]
         contador = 1
         for maquin in maquinas:
-            if maquin == maqAleatoria:
+            if maquin["tipo"] == maqAleatoria:
                 contador+=1
         if contador<=2:
             break
     maquinas.append({'id':i,'tipo':maqAleatoria})
-
 tareas=[{'id':i,"T":f"Tarea_{i}",'M':maquinas[randint(0,len(maquinas)-1)]["tipo"],'D':randint(1,max_duracionTarea)} for i in range(cant_tareas)]
 
 tiempo_max=0
 for i in range(cant_tareas):
     tiempo_max+=tareas[i]['D']
 
-# Ya tengo maquina, tareas y tiempo_max
+# Ya tengo maquinas, tareas y tiempo_max
 
 
 print("FIN")
-# Satisfaccion de restricciones
-
-# Problema: ====================================================
-
-# al inicio hay que definir una lista de tareas
-# cada TAREA tiene:
-# - Identificacion
-# - Duracion
-# - Requerimientos de Maquina (Tipo)
-
-# ademas hay que cargar las maquinas que tenemos
-# cada MAQUINA tiene:
-# - Identificador
-# - Tipo
 
 # Variables: ====================================================
 # TSi: periodo en que se inicia la tarea (num entero de periodos ej horas)
