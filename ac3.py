49e31e7077ffb2681d310324e8c1ecb97764ec0c
lucas-trubiano <lucas.trubiano@gmail.com>
2021-06-03 10:21:32 -0300

    comitear

--- ac3.py
+++ ac3.py
@@ -6,9 +6,10 @@ class csp():
         self.C = []
 
 def ac3(csp): #receive binary CSP (X,D,C)
-    queue = [] # cola de arcos, inicialmente los arcos en el csp
+    queue = csp.C # cola de arcos, inicialmente los arcos en el csp
+            # []
     while len(queue)>0:
-        Xi,Xj = queue.pop(0) # saco el primer elemento
+        Xi,Xj = queue.pop(0)["alcance"] # saco el primer elemento
         if revise(csp,Xi,Xj):
             if len(csp.Di)==0:
                 return False
@@ -16,9 +17,9 @@ def ac3(csp): #receive binary CSP (X,D,C)
                     pass #
     return True
 
-def revise(csp,Xi,i,Xj,j):
+def revise(csp,Xi,Xj):
     revised = False
-    for x in csp.D[i]:
+    for x in csp.D[i]: # (m1,1)....,(m3,1)...
         hay_y = 
         # hay un "y" en "Dj" que cumpla que (x,y) satisface restricciones entre (Xi,Xj)
         # sino, eliminar ese x del dominio Di y poner revised como true
