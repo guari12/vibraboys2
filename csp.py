1ab1c902f36130e06ab8b60352c6f4095f092310
guari12 <renzoguari99@gmail.com>
2021-06-08 17:27:49 -0300

    qa

--- csp.py
+++ csp.py
@@ -35,9 +35,6 @@ class grafo_csp():
 
     def constraint(self):
         print("--- Creando restricciones")
-        # ejemplo
-        # C = {"id1":{"id3":[],
-        #             "id5":{"m1,1":[]}}}
         
         for i in range(len(self.X)): # recorro variables TSMi
             X1 = self.X[i]
@@ -51,7 +48,11 @@ class grafo_csp():
                     keyB = X2["Tarea"] # id de la tarea
                      
                     if self.tareas[i]["M"]==self.tareas[j]["M"]:
-                                                     #Cij
+                        try: # esto es muy importante
+                            self.C[keyA].update({ keyB:{} }) # trata de agregar una restriccione
+                        except: # pero si no estan creadas
+                            self.C.update({ keyA:{   keyB:  {}    } }) # entonces las crea
+                                                       #Cij
                         Cij = {}
 
                         Dominio1 = self.D[i]["Dominio"]
@@ -82,65 +83,14 @@ class grafo_csp():
             if len(Bij)>0:               
                 self.C.update({keyA:Bij}) # lista de valores posibles en la tarea B
     
-    #funcion que setea el dominio temporal entre tareas
-    def set_dominio(self,tareas,ids_maquinas,cant_maquinas):
-        #Vector que contendra cada una de las posibles restricciones entre dos variables
-        rest={"id_tarea":'',"id_maq":'',"t_inicio":'',"t_fin":''}
-        restrictions=[]
-        n=len(tareas)
-        for x in range(n):
-            dominio=self.D
-            for k in dominio:
-                if (k%tareas[0]["D"])==0:
-                    aux=k-tareas[0]["D"]
-                    rest["id_tarea"]=tareas[0]["id"]
-                    rest["id_maq"]=ids_maquinas[0]
-                    rest["t_inicio"]=aux
-                    rest["t_fin"]=aux + tareas[0]["D"]
-                    restrictions.append(rest)
-
-                    #En este caso tarea 1 contendra un unico valor, y tarea 2 ira tomando cada uno de los 
-                    #valores posibles en el dominio restante
-                    if len(tareas)==2 and len(tareas)>cant_maquinas:
-                        #Se eliminan los valores del dominio ocupados por la tarea 1
-                        dominio_aux=self.D
-                        for j in range(rest["t_inicio"],rest["t_fin"]):
-                            dominio_aux.pop(j)
-
-                        #Se recorre el nuevo dominio en busca de tiempos suficientes para ejecutar la tarea 2
-                        for i in dominio_aux:
-                            if (i%tareas[1]["D"])==0:
-                                aux=i-tareas[1]["D"]
-                                rest["id_tarea"]=tareas[1]["id"]
-                                rest["id_maq"]=ids_maquinas[1]
-                                rest["t_inicio"]=aux
-                                rest["t_fin"]=aux + tareas[1]["D"]
-                                restrictions.append(rest)
-
-                    #Caso hipotetico de dos tareas que disponen de dos maquinas del mismo tipo
-                    elif len(tareas)==2 and len(tareas)==cant_maquinas:
-                        for p in dominio:
-                            if (p%tareas[1]["D"])==0:
-                                aux=p-tareas[1]["D"]
-                                rest["id_tarea"]=tareas[1]["id"]
-                                rest["id_maq"]=ids_maquinas[1]
-                                rest["t_inicio"]=aux
-                                rest["t_fin"]=aux + tareas[1]["D"]
-                                restrictions.append(rest)
-            tareas.reverse()
-            ids_maquinas.reverse()
-        return restrictions
+    def getArcos(self):
+        arcos = []
+        for key1 in self.C:
+            for key2 in self.C[key1]:
+                arcos.append((key1,key2))
+        return arcos
     
-    def get_idmaq(self,maquina,cant_maq):
-        ids=[]
-        for j in range(cant_maq):
-            i=0
-            while maquina!=self.maquinas[i]:
-                i+=1
-            ids.append(self.maquinas[i]["id"])
-            self.maquinas.pop(i)
-        return ids
-
+    #funcion que setea el dominio temporal entre tareas
+    
     def __str__(self):
         return "Variables:\n%s\nDominio temporal:\n%s\nRestricciones:\n%s\n" % (self.X, self.D,self.C)
-
